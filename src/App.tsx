99ca61fab01a4d7a7b8f29feb46385ae7e7a5db4
ametsuramet <ametsuramet@gmail.com>
2024-12-23 11:25:08 +0700

    edit app 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          BELAJAR REACT
+          BELAJAR REACT BERSAMA
         </a>
       </header>
     </div>
